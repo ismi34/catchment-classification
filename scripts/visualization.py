57d9f7e4afe3e0696a4ffd859c671b6b6c912f32
Kazi Hossain <kaziismi@gmail.com>
2025-07-27 14:21:34 +0200

    visualization 2.0

---
+++ scripts/visualization.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+import os
+
+def bar_plot(dtframe, outpath, outfilename):
+    outpath = Path(outpath)
+    x = dtframe['catchment']
+    y = dtframe['distance']
+    title = 'Similarity Comparison of Catchments: Altotting to Others\nBased on Landcover Classes'
+    xlabel = 'Catchment ID'
+    ylabel = 'Distance (Similarity Index)'
+    fig, ax = plt.subplots(figsize=(20, 8), facecolor='#f7f7f7')  # White background
+
+    ax.bar(x, y, width=0.6, color='steelblue')
+    ax.set_title(title, fontsize=18, fontweight='bold')
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_facecolor('white')
+
+
+    ax.set_xticks(range(len(x)))
+    ax.set_xticklabels(x, rotation=90, fontsize=10)
+
+    # Dotted grid line
+    ax.grid(axis='y', linestyle=':', color='gray')
+
+    for spine in ax.spines.values():
+        spine.set_visible(True)
+        spine.set_color('black')
+        spine.set_linewidth(1)
+
+    fig.tight_layout()
+    fig.savefig(outpath/outfilename, dpi=300)
+    plt.show()
